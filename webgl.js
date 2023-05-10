a19d561967fd080e3fe95fae3548b900526ae5a4
Dr John O'Hare <github@xrsystems.uk>
2023-05-10 19:16:45 +0100

    Update webgl.js

--- webgl.js
+++ webgl.js
@@ -1,22 +1,49 @@
+// A helper function to create a WebGL context
+function createContext(canvas) {
+  const context = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+  if (!context) {
+    throw new Error('WebGL not supported');
+  }
+  return context;
+}
+
+// A helper function to create a WebGL shader
+function createShader(gl, type, source) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    throw new Error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+  }
+
+  return shader;
+}
+
+// A helper function to create a WebGL program
+function createProgram(gl, vertexShader, fragmentShader) {
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    throw new Error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(program));
+  }
+
+  return program;
+}
+
 const canvas = document.getElementById('webgl-canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const gl = canvas.getContext('webgl2');
-
-if (!gl) {
-  alert('WebGL 2 not supported by your browser');
-}
-
+const gl = createContext(canvas);
 gl.clearColor(0.5, 1.0, 0.5, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 gl.enable(gl.DEPTH_TEST);
 
-gl.viewport(0, 0, canvas.width, canvas.height);
-
 const vertexShaderSource = `
-  #version 300 es
-  in vec4 coordinates;
+  attribute vec4 coordinates;
   uniform mat4 u_transform;
   void main() {
     gl_Position = u_transform * coordinates;
@@ -24,56 +51,45 @@ const vertexShaderSource = `
 `;
 
 const fragmentShaderSource = `
-  #version 300 es
   precision mediump float;
-  out vec4 fragColor;
   void main() {
-    fragColor = vec4(0.3, 0.3, 0.3, 1.0);
+    gl_FragColor = vec4(0.3, 0.3, 0.3, 1.0);
   }
 `;
 
-const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertexShader, vertexShaderSource);
-gl.compileShader(vertexShader);
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+const program = createProgram(gl, vertexShader, fragmentShader);
 
-const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragmentShader, fragmentShaderSource);
-gl.compileShader(fragmentShader);
+gl.useProgram(program);
 
-const shaderProgram = gl.createProgram();
-gl.attachShader(shaderProgram, vertexShader);
-gl.attachShader(shaderProgram, fragmentShader);
-gl.linkProgram(shaderProgram);
+const vertices = new Float32Array([
+  0.0, -0.5, -0.5,
+  0.0,  0.5,  0.0,
+  0.5, -0.5,  0.5,
+  -0.5, -0.5,  0.5
+]);
 
-gl.useProgram(shaderProgram);
-
-const vertices = [
-  0.0, -0.25, -0.50,
-  0.0,  0.25,  0.00,
-  0.5, -0.25,  0.25,
-  -0.5, -0.25,  0.25
-];
-
-const indices = [
+const indices = new Uint16Array([
   2, 1, 3,
   3, 1, 0,
   0, 1, 2,
   0, 2, 3
-];
+]);
 
 const vertexBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-const indexBuffer = gl.createBuffer();
+constindexBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 
-const coordinates = gl.getAttribLocation(shaderProgram, 'coordinates');
+const coordinates = gl.getAttribLocation(program, 'coordinates');
 gl.vertexAttribPointer(coordinates, 3, gl.FLOAT, false, 0, 0);
 gl.enableVertexAttribArray(coordinates);
 
-const transformUniformLocation = gl.getUniformLocation(shaderProgram, 'u_transform');
+const transformUniformLocation = gl.getUniformLocation(program, 'u_transform');
 let rotationAngle = 0;
 
 function animate() {
