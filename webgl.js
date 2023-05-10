14926df2a9fa0ddb8263bb8eea0a3fa36548ff17
Dr John O'Hare <github@xrsystems.uk>
2023-05-10 20:54:35 +0100

    Update webgl.js

--- webgl.js
+++ webgl.js
@@ -1,6 +1,6 @@
 // Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x7fff7f);
+scene.background = new THREE.Color(0xbf9000);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
