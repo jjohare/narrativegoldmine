d10ddb34a6008cce109a439d538c2764954a218b
Dr John O'Hare <github@xrsystems.uk>
2023-05-10 20:45:48 +0100

    Update webgl.js

--- webgl.js
+++ webgl.js
@@ -1,66 +1,52 @@
-// Helper function to create a colored bar between two points
-function createColorCyclingBar(start, end, colorSpeed) {
-  const direction = new THREE.Vector3().subVectors(end, start);
-  const length = direction.length();
-  direction.normalize();
+// Set up the scene, camera, and renderer
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x7fff7f);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-  const quaternion = new THREE.Quaternion();
-  quaternion.setFromUnitVectors(new THREE.Vector3(0, 1, 0), direction);
+// Create a pyramid using a custom buffer geometry
+const geometry = new THREE.BufferGeometry();
+const vertices = new Float32Array([
+  0, 1, 0,
+  -1, -1, 1,
+  1, -1, 1,
+  1, -1, -1,
+  -1, -1, -1
+]);
+const indices = new Uint32Array([
+  0, 1, 2,
+  0, 2, 3,
+  0, 3, 4,
+  0, 4, 1,
+  1, 4, 3,
+  1, 3, 2
+]);
 
-  const geometry = new THREE.CylinderGeometry(0.05, 0.05, length, 8);
-  const material = new THREE.MeshBasicMaterial();
+geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+geometry.setIndex(new THREE.BufferAttribute(indices, 1));
 
-  const bar = new THREE.Mesh(geometry, material);
-  bar.position.copy(start).lerp(end, 0.5);
-  bar.setRotationFromQuaternion(quaternion);
+// Create a dark grey material
+const material = new THREE.MeshBasicMaterial({ color: 0x4B4B4B });
 
-  bar.userData.colorSpeed = colorSpeed;
-  return bar;
-}
+// Create the pyramid mesh and add it to the scene
+const pyramid = new THREE.Mesh(geometry, material);
+scene.add(pyramid);
 
-// Create the pyramid edges using color cycling bars
-const pointA = new THREE.Vector3(0, 1, 0);
-const pointB = new THREE.Vector3(-1, -1, 1);
-const pointC = new THREE.Vector3(1, -1, 1);
-const pointD = new THREE.Vector3(1, -1, -1);
-const pointE = new THREE.Vector3(-1, -1, -1);
+// Set the camera position
+camera.position.z = 5;
 
-const edges = [
-  createColorCyclingBar(pointA, pointB, 0.01),
-  createColorCyclingBar(pointA, pointC, 0.02),
-  createColorCyclingBar(pointA, pointD, 0.03),
-  createColorCyclingBar(pointA, pointE, 0.04),
-  createColorCyclingBar(pointB, pointC, 0.05),
-  createColorCyclingBar(pointC, pointD, 0.06),
-  createColorCyclingBar(pointD, pointE, 0.07),
-  createColorCyclingBar(pointE, pointB, 0.08),
-];
-
-edges.forEach(edge => scene.add(edge));
-
-// Update colors for the color cycling bars
-function updateEdgeColors() {
-  const hsl = new THREE.Color();
-
-  edges.forEach(edge => {
-    hsl.setHSL((hsl.getHSL().h + edge.userData.colorSpeed) % 1, 1, 0.5);
-    edge.material.color.copy(hsl);
-  });
-}
-
-// Update the animation loop
+// Set up the animation loop
 function animate() {
   requestAnimationFrame(animate);
 
-  // Update edge colors
-  updateEdgeColors();
-
-  // Rotate the edges
-  edges.forEach(edge => {
-    edge.rotation.x += 0.01;
-    edge.rotation.y += 0.01;
-  });
+  // Rotate the pyramid
+  pyramid.rotation.x += 0.01;
+  pyramid.rotation.y += 0.01;
 
   // Render the scene
   renderer.render(scene, camera);
 }
+
+animate();
